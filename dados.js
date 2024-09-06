f9f791534f67ffc4a6537eb276cf24912adc334c
marcelojuniormaral9878 <marceloamara9878@gmail.com>
2024-09-06 14:14:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "neymar junior",
+        descricao: "Neymar da Silva Santos Júnior (Mogi das Cruzes, 5 de fevereiro de 1992) é um futebolista brasileiro que atua como atacante. Atualmente joga no Al-Hilal, da Arábia Saudita.",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "jogador de futebol"
+    },
+    {
+        titulo: "Flávio Caça-Rato",
+        descricao: "Flávio Augusto do Nascimento, mais conhecido como Flávio Caça-Rato, ou simplesmente Caça-Rato, (Recife, 29 de junho de 1986), é um futebolista brasileiro que atua como atacante. Atualmente, está sem clube",
+        link: "https://pt.wikipedia.org/wiki/Fl%C3%A1vio_Ca%C3%A7a-Rato",
+        tags: "jocador de futebol"
+    },
+    {
+        titulo: "cristiano ronaldo",
+        descricao: "Cristiano Ronaldo dos Santos Aveiro OIH • GOIH • ComM (Funchal, 5 de fevereiro de 1985) é um futebolista português que atua como ponta-esquerda ou avançado.",
+        link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+        tags: "jogador de futebol"
+    }
+];
